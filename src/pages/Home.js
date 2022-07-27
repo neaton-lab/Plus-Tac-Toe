265f3be1df269051aa2872b2504fb416aa2be3df
Demir Altay <demiraltay@my.unt.edu>
2022-07-27 12:05:49 -0500

    Fixed help and settings button alignment on smaller screens

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -46,7 +46,7 @@ function Home() {
             </Link>
 
             <Link to="/help"> {/* link to help page. Icon from Hero Icons Icon-Question-Mark-Circle */}
-              <button className='absolute translate-x-14 md:-translate-x-2 bottom-0 md:bottom-[50px] text-center right-0 text-gray-700  bg-gray-400 hover:bg-gray-500 px-2 py-2 md:px-3 md:py-3 m-5 mr-0 rounded-full'>
+              <button className='absolute translate-x-12 md:-translate-x-2 bottom-0 md:bottom-[50px] text-center right-0 text-gray-700  bg-gray-400 hover:bg-gray-500 px-2 py-2 md:px-3 md:py-3 m-5 mr-0 rounded-full'>
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
